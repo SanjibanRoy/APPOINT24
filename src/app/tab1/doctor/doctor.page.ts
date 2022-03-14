f7c01a02e3bb40305b0b1e9e1c5ae76a932c2ee0
sanjiban234 <56555911+sanjiban234@users.noreply.github.com>
2022-03-15 00:34:22 +0530

    master

--- src/app/tab1/doctor/doctor.page.ts
+++ src/app/tab1/doctor/doctor.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-doctor',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorPage implements OnInit {
 
-  constructor() { }
+  constructor(public alertController: AlertController) { }
 
   ngOnInit() {
   }
@@ -16,4 +17,62 @@ export class DoctorPage implements OnInit {
     loop: true,
     spaceBetween: -100,
   };
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Choose a Date',
+      inputs: [
+        
+        {
+          name: 'PLease Select a Date',
+          type: 'date',
+          min: '2017-03-01',
+          max: '2018-01-12'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+  // async presentAlertConfirm() {
+  //   const alert = await this.alertController.create({
+  //     cssClass: 'my-custom-class',
+  //     header: 'Confirm!',
+  //     message: 'Are your sure ',
+  //     buttons: [
+  //       {
+  //         text: 'Cancel',
+  //         role: 'cancel',
+  //         cssClass: 'secondary',
+  //         id: 'cancel-button',
+  //         handler: (blah) => {
+  //           console.log('Confirm Cancel: blah');
+  //         }
+  //       }, {
+  //         text: 'Okay',
+  //         id: 'confirm-button',
+  //         handler: () => {
+  //           console.log('Confirm Okay');
+  //         }
+  //       }
+  //     ]
+  //   });
+
+  //   await alert.present();
+  // }
 }
